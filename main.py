c194a18d58bfe394847616626a21fe18c5372a44
Divider <89965004+it-cio@users.noreply.github.com>
2021-11-25 23:29:43 +0300

    add description

--- main.py
+++ main.py
@@ -9,24 +9,24 @@ app = FastAPI()
 security = HTTPBasic()
 
 
-@app.get("/authorization")
+@app.get("/authorization")  # basic authorization
 def users(credentials: HTTPBasicCredentials = Depends(security)):
     return {"username": credentials.username, "password": credentials.password}
 
 
-@app.post("/upload")
+@app.post("/upload")  # image loader
 async def image(files: List[UploadFile] = File(...)):
     save_list = []
     for img in files:
-        if 'image' in img.content_type and len(img.file.read()) < 200000:
-            if not os.path.isdir('img'):
-                os.mkdir('img')
-            with open(f"img/{img.filename}", "wb") as buffer:
-                shutil.copyfileobj(img.file, buffer)
+        if 'image' in img.content_type and len(img.file.read()) < 200000:  # check for content is image and image size not above 200kb
+            if not os.path.isdir('img'):  # check if in root directory exist 'img' folder
+                os.mkdir('img')  # if 'img' folder not exist create him
+            with open(f"img/{img.filename}", "wb") as buffer:  # save content in buffer
+                shutil.copyfileobj(img.file, buffer)  # save file in 'img' folder
             save_list.append(img.filename)
 
     return {"saved_files": save_list}
 
 
-if __name__ == '__main__':
+if __name__ == '__main__':  # start local uvicorn server
     uvicorn.run('main:app', reload=True)
