5fb7750399b50a9720e4acae17386a0773a1c1c2
Divider <89965004+it-cio@users.noreply.github.com>
2021-11-26 00:09:15 +0300

    fix authorisation

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
 import os
 import shutil
 import uvicorn
+import secrets
 from typing import List
-from fastapi import FastAPI, Depends, UploadFile, File
+from fastapi import FastAPI, Depends, HTTPException, status, UploadFile, File
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 
 app = FastAPI()
 security = HTTPBasic()
 
 
-@app.get("/authorization")  # basic authorization
-def users(credentials: HTTPBasicCredentials = Depends(security)):
-    return {"username": credentials.username, "password": credentials.password}
+def get_current_username(credentials: HTTPBasicCredentials = Depends(security)):
+    correct_username = secrets.compare_digest(credentials.username, "Tester")  # simple username and password
+    correct_password = secrets.compare_digest(credentials.password, "qwerty123")  # to pass verification
+    if not (correct_username and correct_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect name or password",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+    return credentials.username
 
 
 @app.post("/upload")  # image loader
-async def image(files: List[UploadFile] = File(...)):
+async def image(files: List[UploadFile] = File(...), username: str = Depends(get_current_username)):
     save_list = []
     for img in files:
         if 'image' in img.content_type and len(img.file.read()) < 200000:  # check for content is image and image size not above 200kb
@@ -24,8 +32,7 @@ async def image(files: List[UploadFile] = File(...)):
             with open(f"img/{img.filename}", "wb") as buffer:  # save content in buffer
                 shutil.copyfileobj(img.file, buffer)  # save file in 'img' folder
             save_list.append(img.filename)
-
-    return {"saved_files": save_list}
+    return {f"{username} saved_files": save_list}
 
 
 if __name__ == '__main__':  # start local uvicorn server
